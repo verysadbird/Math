13973832c22e0d58f97534362d1edfec6915420b
verysadbird <77414505+verysadbird@users.noreply.github.com>
2021-01-19 12:34:38 +0300

    Add project files.

---
+++ Calculations.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Math
+{
+    class Calculations
+    {
+
+        public void Adding(int a, int b)
+        {
+
+            int solution = a + b;
+            Console.WriteLine("Result: " + solution);
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Math
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Calculations cal = new Calculations();
+            cal.Adding(5, 8);
+            cal.Adding(89, 12);
+            cal.Adding(77, 1);
+
+
+        }
+
+
+    }
+}
